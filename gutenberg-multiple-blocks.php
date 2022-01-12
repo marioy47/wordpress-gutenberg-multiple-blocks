7836e52e513901468b2084c31d2a35eaa226613e
Mario Yepes <marioy47@gmail.com>
2022-01-12 17:34:50 -0500

    First commit

---
+++ gutenberg-multiple-blocks.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Gutenberg Multiple Blocks Example
+ *
+ * @package           GutenBlocks
+ * @author            Mario Yepes
+ * @copyright         2022 Mario Yepes
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Gutenberg Multiple Blocks Example
+ * Plugin URI:        https://marioyepes.com
+ * Description:       Tutorial on how to add multiple blocks to one Gutenbert plugin
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Author:            Mario Yepes
+ * Author URI:        https://marioyepes.com
+ * Text Domain:       gutenberg-multiple-blocks
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
+
