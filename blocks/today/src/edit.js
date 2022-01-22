b6fe657772b13e5d9751eb8a5ed10936a334470b
Mario Yepes <marioy47@gmail.com>
2022-01-22 12:30:49 -0500

    Styling the block in the backend

--- blocks/today/src/edit.js
+++ blocks/today/src/edit.js
@@ -1,14 +1,18 @@
 // blocks/today/src/edit.js
 
 import {__} from "@wordpress/i18n";
+import "./style.scss"
 
 const Edit = (params) => {
+	const {className, isSelected} = params;
 	return (
 		<>
-			<h3>{__("Block parammeters", "gutenberg-multi")}</h3>
-			<pre>{JSON.stringify(params, null, 4)}</pre>
-			<h3>{__("Passed functions:", "gutenberg-multi")}</h3>
-			<p>Type of setAttributes: {typeof params.setAttributes}</p>
+			<div className={isSelected ? `${className} selected`: className}>
+				<h3>{__("Block parammeters", "gutenberg-multi")}</h3>
+				<pre>{JSON.stringify(params, null, 4)}</pre>
+				<h3>{__("Passed functions:", "gutenberg-multi")}</h3>
+				<p>Type of setAttributes: {typeof params.setAttributes}</p>
+			</div>
 		</>
 	);
 };
