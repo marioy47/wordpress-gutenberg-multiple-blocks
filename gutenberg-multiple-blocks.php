2bdb15644e64e2ff87cd7bac3333e192a5873bf2
Mario Yepes <marioy47@gmail.com>
2022-01-23 22:43:45 -0500

    Fixing name change

--- gutenberg-multiple-blocks.php
+++ gutenberg-multiple-blocks.php
@@ -23,32 +23,29 @@
  */
 
 function register_user_form_block() {
-	$assets = include_once __DIR__ . '/blocks/user-form/build/index.asset.php';
+	$assets = include_once __DIR__ . '/blocks/today/build/index.asset.php';
 
 	wp_register_script(
-		'guten-multi-user-form',
-		plugins_url('./blocks/user-form/build/index.js', __FILE__),
+		'gutenberg-multi-today',
+		plugins_url('./blocks/today/build/index.js', __FILE__),
 		$assets['dependencies'],
 		$assets['version']
 	);
 
 	wp_register_style(
-		'guten-multi-user-form',
-		plugins_url('./blocks/user-form/build/style-index.css', __FILE__),
+		'gutenberg-multi-today',
+		plugins_url('./blocks/today/build/index.css', __FILE__),
 		array(),
 		$assets['version']
 	);
 
 	register_block_type(
-		'guten-multi/user-form',
+		'gutenberg-multi/today',
 		array(
-			'editor_script' => 'guten-multi-user-form',
-			'style' => 'guten-multi-user-form'
+			'editor_script' => 'gutenberg-multi-today',
+			'style' => 'gutenberg-multi-today',
 		)
 	);
 }
 
 add_action('init', 'register_user_form_block');
-
-
-
